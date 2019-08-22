6e18cca43966603ad4c0950de734b9096dc626b7
G s s Venkatesh <gssvenkatesh92@gmail.com>
2019-08-22 18:57:08 +0530

    return state

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -2,5 +2,6 @@ const userReducers = (state = {user: []}, action) => {
     if(action.type === 'ADD_USER_DATA'){
         return { user: [ ...state.user.concat(action.payload) ]} 
     }
+    return state;
 }
 export default userReducers;
